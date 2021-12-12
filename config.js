1513aa8ae6cb9d9f62c2dcafbf883f7ae70af216
tsmoreau <64439028+tsmoreau@users.noreply.github.com>
2021-12-11 19:26:43 -0800

    token

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const sharkTokenAddress = "0x3d61037b7Bd8C5F96c38C1198b8dF62010D75043";
+export const sharkTokenAddress = "0x4D78f6bfd7D613480e79C03B6609FfeC94f10E8B";
 export const sharkCalculatorAddress = "";
 export const sharkAssemblerAddress = "";
 export const sharkStorageAddress = "0x1D76F276a05d4aA4F51D748fBdCB50a2D20e1973";
